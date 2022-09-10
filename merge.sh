aaaf980b94465fb4055a6643398804476b9a7493
SunilKumaruppara <110538291+SunilKumaruppara@users.noreply.github.com>
2022-09-10 11:35:45 +0530

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd  /var/lib/jenkins/workspace/$JOB_NAME/tata-project
 git status
-git checkout master
-git merge origin/jenkins-demo 
+git tag -a tagName -m "Your tag comment"
+git merge jenkins-demo
+git commit -am "Merged jenkins-demo branch to master'
+git push origin master
