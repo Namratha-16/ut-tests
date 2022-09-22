dc70426bdd7c4adcb1bdf0b4cca7a74fc2b58832
naveen1902 <105701396+naveen1902@users.noreply.github.com>
2022-09-22 19:06:07 +0530

    Create commitmail.py

---
+++ commitmail.py
@@ -0,0 +1,15 @@
+c=input(" ")
+d=c.split()
+mailid={"SunilKumaruppara":"upparasunil.k@tataelxsi.co.in","naveen1902":"ba.pillanaveen@tataelxsi.co.in","harsha27838":"seelam.r@tataelxsi.co.in","Namratha-16":"namrathanagm999@gmail.com"}
+for i in d:
+  print(mailid[i])
+
+
+
+
+
+
+
+
+
+
