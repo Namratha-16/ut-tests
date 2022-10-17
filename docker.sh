62defa3535e3ef73de97d4fd02c11a1e416f9a28
SunilKumaruppara <110538291+SunilKumaruppara@users.noreply.github.com>
2022-10-17 19:00:10 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t polaris-slave03:v3.0 --build-arg ssh_prv_key="$(cat id_rsa)" --build-arg ssh_pub_key="$(cat id_rsa.pub)"  .
+docker build -t polaris-slave03:$image_version --build-arg ssh_prv_key="$(cat id_rsa)" --build-arg ssh_pub_key="$(cat id_rsa.pub)"  .
