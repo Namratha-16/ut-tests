e3a51446a37f2c6a55e293cbe150475cdc9b4063
SunilKumaruppara <110538291+SunilKumaruppara@users.noreply.github.com>
2022-10-18 11:18:51 +0530

    Update kodiakbuild.sh

--- kodiakbuild.sh
+++ kodiakbuild.sh
@@ -6,7 +6,9 @@ cd conf/
 sed -i 's/#MACHINE = "samsung-initial"/MACHINE = "samsung-initial"/g' local.conf
 sed -i 's/#VENUS_PRODUCT = "orv-7in"/VENUS_PRODUCT = "orv-7in"/g' local.conf
 sed -i 's/SSTATE_DIR/#SSTATE_DIR/g' local.conf
-sed -i 's/#EXTRA_IMAGE_FEATURES/EXTRA_IMAGE_FEATURES/g' local.conf
+sed -i 's/DL_DIR/#DL_DIR/g' local.conf
+echo "DL_DIR=/home/jenkins/work/downloads" >local.conf
+#sed -i 's/#EXTRA_IMAGE_FEATURES/EXTRA_IMAGE_FEATURES/g' local.conf
 cd ../
 
 bitbake core-image-samsung
