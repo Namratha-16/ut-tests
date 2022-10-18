1cdedb96f66f1444f012e24042c427062e6c5736
SunilKumaruppara <110538291+SunilKumaruppara@users.noreply.github.com>
2022-10-18 14:35:12 +0530

    Update kodiakbuild.sh

--- kodiakbuild.sh
+++ kodiakbuild.sh
@@ -1,16 +1,16 @@
 #!/bin/bash/
 cd /var/lib/jenkins/workspace/$JOB_NAME
 cd kodiak
-source build_init
+. ./build_init
 cd conf/
-sed -i 's/#MACHINE = "samsung-initial"/MACHINE = "samsung-initial"/g' local.conf
+sed -i 's/#MACHINE = "polaris-9210"/MACHINE = "polaris-9210"/g' local.conf
 sed -i 's/#VENUS_PRODUCT = "orv-7in"/VENUS_PRODUCT = "orv-7in"/g' local.conf
 sed -i 's/SSTATE_DIR/#SSTATE_DIR/g' local.conf
-sed -i 's/DL_DIR/#DL_DIR/g' local.conf
-echo "DL_DIR=/home/jenkins/work/downloads" >local.conf
+#sed -i 's/DL_DIR/#DL_DIR/g' local.conf
+#echo "DL_DIR=/home/jenkins/work/downloads" >local.conf
 #sed -i 's/#EXTRA_IMAGE_FEATURES/EXTRA_IMAGE_FEATURES/g' local.conf
 cd ../
 
-bitbake core-image-samsung
+bitbake venus
 
 
