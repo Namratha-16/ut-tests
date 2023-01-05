2051051b24ca54093e03b447acafdf51817f5af5
SunilKumaruppara <110538291+SunilKumaruppara@users.noreply.github.com>
2023-01-05 15:13:39 +0530

    Update kodiakbuild.sh

--- kodiakbuild.sh
+++ kodiakbuild.sh
@@ -1,17 +1,3 @@
 #!/bin/bash/
-cd /var/lib/jenkins/workspace/$JOB_NAME
-cd kodiak
-. ./build_init
-pwd
-cd conf/
-sed -i 's/#MACHINE = "polaris-9210"/MACHINE = "polaris-9210"/g' local.conf
-sed -i 's/#VENUS_PRODUCT = "orv-7in"/VENUS_PRODUCT = "orv-7in"/g' local.conf
-sed -i 's/SSTATE_DIR/#SSTATE_DIR/g' local.conf
-#sed -i 's/DL_DIR/#DL_DIR/g' local.conf
-#echo "DL_DIR=/home/jenkins/work/downloads" >local.conf
-#sed -i 's/#EXTRA_IMAGE_FEATURES/EXTRA_IMAGE_FEATURES/g' local.conf
-cd ../
-bitbake -c clean u-boot-fslc
-bitbake venus
-
-
+cd /var/lib/jenkins/workspace/$JOB_NAME/kodiak_$BUILD_NUMBER
+mkdir testing
